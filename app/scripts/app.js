5fcfd79e0b88eeec8b8455638f0603841c2f5968
mizi <mizi.lin@admaster.com.cn>
2016-01-06 12:28:30 +0800

    adjust page

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,30 +1,36 @@
-// Ionic Starter App
-
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
-    //
-    //.run(function($ionicPlatform) {
-    //  $ionicPlatform.ready(function() {
-    //    if(window.cordova && window.cordova.plugins.Keyboard) {
-    //      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    //      // for form inputs)
-    //      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    //
-    //      // Don't remove this line unless you know what you are doing. It stops the viewport
-    //      // from snapping when text inputs are focused. Ionic handles this internally for
-    //      // a much nicer keyboard experience.
-    //      cordova.plugins.Keyboard.disableScroll(true);
-    //    }
-    //    if(window.StatusBar) {
-    //      StatusBar.styleDefault();
-    //    }
-    //  });
-    //})
 
     .constant('app', {
-        version: 'v1.0.0'
+        version: 'v1.0.0',
+        corpid: 'wx44d125e53237e988',
+        corpsecret: 'pobyKsmqh09nWh-EdhAEhwLhNki_Cy_oxdoJtKqHseK0sUGNiPR13K-8PJUbNsW-',
+        'access_token': localStorage.getItem('X_TOKEN') || '',
+        'wx_api': 'https://qyapi.weixin.qq.com/cgi-bin',
+        'wx_api_map': {
+            // 获得用户token
+            '1': '/gettoken',
+            // 获取成员信息
+            '9': '/user/get',
+            // 获得成员列表（简单信息））
+            '10': '/user/simplelist',
+            // 通过code 获得当前用户信息
+            '38': '/user/getuserinfo',
+            // 获得成员列表（详细信息））
+            '59': '/user/list'
+        }
+    })
+
+    .config(function ($provide) {
+        $provide.decorator('$state', function ($delegate, $stateParams) {
+            $delegate.forceReload = function () {
+                return $delegate.go($delegate.current, $stateParams, {
+                    reload: true,
+                    inherit: false,
+                    notify: true
+                });
+            };
+            return $delegate;
+        });
     })
 
     .config(['$httpProvider', function ($httpProvider) {
@@ -36,20 +42,102 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
         // 针对IE，禁止缓存ajax 请求
         $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
         $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
+
+        $httpProvider.interceptors.push(['$q', 'app', function ($q, app) {
+
+            return {
+                request: function (config) {
+                    var reg = /^\/wx$/;
+                    var httpReg = /^https/;
+
+                    config.params = config.params || {};
+
+                    // 判断是否使用 wx debug 接口进行调试应用
+                    if ('POST' === config.method && reg.test(config.url)) {
+                        config.wxapi = true;
+                        config.params.f = 'json';
+                        config.params.access_token = app.access_token;
+
+                        if (httpReg.test(location.href) && config.params.tid) {
+                            config.url = config.wx_api + app['wx_api_map'][config.params.tid];
+                            config.wxapi = false;
+                        }
+                    }
+
+                    return config;
+                },
+
+                response: function (res) {
+                    var rst = res.data, config = res.config, status = res.status;
+
+                    if (200 === status || 204 === status) {
+
+                        if (config.wxapi) {
+                            res.data = JSON.parse(rst.content);
+                            var errcode = res.data.errcode;
+
+                            if (errcode > 0) {
+
+                                if (42001 === errcode || 41001 === errcode) {
+                                    app.$http({
+                                        url: '/wx',
+                                        method: 'POST',
+                                        params: {
+                                            'corpid': app.corpid,
+                                            'corpsecret': app.corpsecret,
+                                            'tid': 1
+                                        }
+                                    }).success(function (data) {
+                                        localStorage.setItem('X_TOKEN', data.access_token);
+                                        app.access_token = data.access_token;
+                                        app.$state.reload();
+                                    });
+                                }
+
+                                return $q.reject(res);
+                            }
+
+                            console.debug('wxapi:::ooOooo', res.data);
+                        }
+
+                        return res;
+                    }
+
+                    return $q.reject(res);
+                },
+
+                responseError: function (res) {
+                    var status = res.status;
+                    return $q.reject(res);
+                }
+
+            };
+        }]);
     }])
 
     .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
 
         $stateProvider
+
             .state('badge', {
                 url: '^/badge',
                 abstract: true
             })
 
+            .state('badge.my', {
+                url: '^/badge/my',
+                views: {
+                    'main@': {
+                        //templateUrl: 'views/badge-members.detail.html',
+                        controller: "CurrentCtrl as CC"
+                    }
+                }
+            })
+
             .state('badge.members', {
                 url: '^/badge/members',
                 views: {
-                    'main@':{
+                    'main@': {
                         templateUrl: 'views/badge-members.html',
                         controller: "MemberCtrl as MC"
                     }
@@ -59,34 +147,46 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
             .state('badge.members.detail', {
                 url: '^/badge/members/:id',
                 views: {
-                    'main@':{
+                    'main@': {
                         templateUrl: 'views/badge-members.detail.html',
                         controller: "MemberDetailCtrl as MDC"
                     }
                 }
             })
 
-
-            .state('badge.me', {
-                url: '^/badge/me/:id',
+            .state('badge.members.detail.records', {
+                url: '^/badge/members/:id/records',
                 views: {
-                    'main@':{
-                        templateUrl: 'views/badge-me.html',
-                        controller: 'BadgeMeCtrl as BMC'
+                    'action@badge.members.detail': {
+                        templateUrl: 'views/member.records.html',
+                        controller: "MemberRecordsCtrl as MRC"
+                    }
+                }
+            })
+
+            .state('badge.members.detail.badgeme', {
+                url: '^/badge/members/:id/badgeme',
+                views: {
+                    'action@badge.members.detail': {
+                        templateUrl: 'views/member.badgeme.html',
+                        controller: 'MemberBadgeMeCtrl as MBMC'
                     }
                 }
             });
 
-            $urlRouterProvider.otherwise('/badge/members');
+        $urlRouterProvider.otherwise('/badge/my');
     }])
 
-    .run(['$injector', 'app', function($injector, app){
+    .run(['$injector', 'app', function ($injector, app) {
         app.$injector = $injector;
         app.$http = $injector.get('$http');
         app.$rootScope = $injector.get('$rootScope');
         app.$location = $injector.get('$location');
         app.$state = $injector.get('$state');
         app.$stateParams = $injector.get('$stateParams');
+
+        app.$rootScope.$state = app.$state;
+        app.$rootScope.$stateParams = app.$stateParams;
 
     }]);
 
--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,14 +1,68 @@
 angular.module('starter.controllers', [])
 
+    // 获取当前用户信息
+    .controller('CurrentCtrl', ['app', function(app){
+        var vm = this;
+
+        var userId = localStorage.getItem('X_USER_ID');
+        var code = localStorage.getItem('X_CODE');
+
+        var my = function(userId){
+            if(userId){
+                app.$state.transitionTo('badge.members.detail.records', {
+                    id: userId
+                }, {
+                    location: 'replace'
+                });
+
+            }else{
+                app.$state.transitionTo('badge.members', {}, {
+                    location: 'replace'
+                });
+            }
+        };
+
+        vm.code = code;
+
+        if(code){
+            app.$http({
+                url: '/wx',
+                method: 'POST',
+                params: {
+                    'code': code,
+                    'agentid': app.corpid,
+                    'tid': 38
+                }
+            }).success(function (data) {
+                var userId = data.userId;
+                localStorage.setItem('X_USER_ID', userId);
+                my(userId);
+            }).error(function(){
+                localStorage.removeItem('X_CODE');
+            });
+        }else{
+            my(userId);
+        }
+
+        return vm;
+    }])
+
     // 首页
     // 用户列表（按徽章数倒序显示)
     .controller('MemberCtrl', ['app', function (app) {
         var vm = this;
 
         app.$http({
-            url: "/api/"
+            url: '/wx',
+            method: 'POST',
+            params: {
+                'department_id': 1,
+                'fetch_child': 1,
+                'status': 0,
+                'tid': 59
+            }
         }).success(function (data) {
-            vm.memberList = data;
+            vm.memberList = data.userlist;
         });
 
         return vm;
@@ -19,21 +73,71 @@ angular.module('starter.controllers', [])
         var vm = this;
 
         app.$http({
-            url: "/api/user/" + app.$stateParams.id
+            url: '/wx',
+            method: 'POST',
+            params: {
+                'userid': app.$stateParams.id,
+                'tid': 9
+            }
         }).success(function (data) {
-            vm.recordList = data.recordList;
-            vm.toUser = data.toUser
+            vm.member = data;
         });
 
         return vm;
 
     }])
 
+    // 用户信息 (关于用户的徽章信息列表)
+    .controller('MemberRecordsCtrl', ['app', function (app) {
+        var vm = this;
+
+        vm.userId = app.$stateParams.id;
+
+        app.$http({
+            url: '/api/user/' + vm.userId
+        }).success(function (data) {
+            vm.recordList = data.recordList;
+        });
+
+        return vm;
+
+    }])
+
+    .controller('MemberBadgeMeCtrl', ['app', function (app) {
+        var vm = this;
+
+        app.$http({
+            url: '/api/badge/all'
+        }).success(function (data) {
+            vm.badgeList = data;
+        });
+
+        vm.form = {
+            toUser: app.$stateParams.id,
+            fromUser: 5,
+            badge: 7
+        };
+
+        vm.submit = function (form) {
+            if (form.$valid) {
+                app.$http({
+                    method: 'POST',
+                    url: '/api/record/add',
+                    data: vm.form
+                }).success(function (data) {
+                    $state.transitionTo('badge.members.detail', {id: vm.toUser}, {location: 'replace'});
+                });
+            }
+        };
+
+        return vm;
+    }])
+
     .controller('BadgeMeCtrl', ['app', function (app) {
         var vm = this;
-        
+
         app.$http({
-            url: "/api/badge/all"
+            url: '/api/badge/all'
         }).success(function (data) {
             vm.badgeList = data;
         });
@@ -44,14 +148,14 @@ angular.module('starter.controllers', [])
             badge: 7
         };
 
-        vm.submit = function(form){
-            if(form.$valid){
+        vm.submit = function (form) {
+            if (form.$valid) {
                 app.$http({
-                    method: "POST",
-                    url: "/api/record/add",
+                    method: 'POST',
+                    url: '/api/record/add',
                     data: vm.form
                 }).success(function (data) {
-                    $state.go('badge.members.detail',{id:vm.toUser});
+                    $state.go('badge.members.detail', {id: vm.toUser});
                 });
             }
         };
